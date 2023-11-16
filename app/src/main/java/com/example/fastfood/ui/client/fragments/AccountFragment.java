132d9d168618a62e1c4d501542a28a7399b995e1
Nguyễn Quý Dương <duongquy2404@gmail.com>
2023-11-16 11:05:58 +0700

    update v2.1

--- app/src/main/java/com/example/fastfood/ui/client/fragments/AccountFragment.java
+++ app/src/main/java/com/example/fastfood/ui/client/fragments/AccountFragment.java
@@ -1,12 +1,25 @@
 package com.example.fastfood.ui.client.fragments;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,27 +27,53 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.Manifest;
 import com.example.fastfood.R;
 import com.example.fastfood.data.controller.UserController;
 import com.example.fastfood.data.model.User;
-import com.example.fastfood.data.reponse.UserResponse;
-import com.example.fastfood.ui.client.activities.ClientActivity;
 import com.example.fastfood.ui.client.activities.LoginActivity;
-import com.example.fastfood.ui.client.activities.RegisterActivity;
 import com.example.fastfood.utils.SessionManager;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AccountFragment extends Fragment {
+    public static final String TAG = AccountFragment.class.getName();
+    private static final int MY_REQUEST_CODE = 10;
     private TextView tvChinhsua;
     private ImageView imgCustomer;
     private TextView tvName;
     private Button btnLogoutC;
     private UserController userController;
     private SessionManager sessionManager;
+
+    private ActivityResultLauncher<Intent> mActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.e(TAG, "onActivityResult");
+                    if(result.getResultCode()== Activity.RESULT_OK){
+                        Intent data=result.getData();
+                        if(data==null){
+                            return;
+                        }
+                        Uri uri=data.getData();
+                        try {
+                            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), uri);
+                            imgCustomer.setImageBitmap(bitmap);
+                        }
+                        catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +97,12 @@ public class AccountFragment extends Fragment {
     }
 
     public void clickEvent(){
+        imgCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickRequestPermission();
+            }
+        });
         btnLogoutC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,7 +129,6 @@ public class AccountFragment extends Fragment {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 if (response.isSuccessful()) {
-                    //UserResponse userResponse = response.body();
                     User user=response.body();
                     tvName.setText(user.getName());
                 }
@@ -98,5 +142,35 @@ public class AccountFragment extends Fragment {
 
             }
         });
+    }
+
+    private void onClickRequestPermission(){
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            openGallery();
+            return;
+        }
+        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            openGallery();
+        }else {
+            String[] permission={Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permission, MY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==MY_REQUEST_CODE){
+            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(intent.createChooser(intent,"Select Picture"));
     }
 }
